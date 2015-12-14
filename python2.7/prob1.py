73e5b248ff76f7aa43b58fea836122a4036aee4f
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-12-14 21:35:32 +0330

    Added a python2.7 solution for problem #1

---
+++ python2.7/prob1.py
@@ -0,0 +1,11 @@
+
+
+nums = {}
+intex = 1
+
+for num in map(int, raw_input().split()):
+    if num not in nums:
+        nums[num] = index
+        index += 1
+    else:
+        print "{} is a duplicate".format(num)
